27410a31eb8ecd2f233542a19ee42084518b304f
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-05-04 19:16:14 -0600

    tasks list

---
+++ src/components/TaskList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export function TaskList(){
+   const handleClick = e => {
+    // alert("hello");
+    const element = e.target;
+    element.element.classList.toggle("crossed-line");
+  };
+  return (
+    <div className="task-container">
+      <div className="task-box">
+        <p style={{width:"14.1em"}}><input style={{marginRight:"15px"}}  type="checkbox"/><input className="task-input" type="text" placeholder="Task name"/><span style={{ paddingLeft:"9px"}}>25</span></p>
+      </div>
+      <div className="task-box">
+        <p style={{width:"14.1em"}}><input style={{marginRight:"15px"}} type="checkbox"/><input className="task-input" type="text" placeholder="Task name"/><span style={{ paddingLeft:"9px"}}>25</span></p>
+      </div>
+      <div className="task-box">
+        <p style={{width:"14.1em"}}><input style={{marginRight:"15px"}} type="checkbox"/><input className="task-input" type="text" placeholder="Task name"/><span style={{ paddingLeft:"9px"}}>25</span></p>
+      </div>
+    </div>
+  );
+}
+export default TaskList;
