39c646f093351147e90b77f3d6010351a14690bb
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-05-08 23:15:59 -0600

    added 'add task button' and 'remove tasks' button

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -17,6 +17,12 @@ export function TaskList(){
       <div className="task-box">
         <p style={{width:"14.1em"}}><input style={{marginRight:"15px"}} type="checkbox"/><input className="task-input" type="text" placeholder="Task name"/><span style={{ paddingLeft:"9px"}}>25</span></p>
       </div>
+      <div>
+        <button >Add Task</button>
+      </div>
+      <div class="">
+        <button style={{width:"fit-content"}}>Remove Finished Tasks</button>
+      </div>
     </div>
   );
 }
