194f6e38cc7178490658f68db8cf2718011ee8be
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-02-27 19:43:16 -0700

    express index file

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-//import React from 'react';
-
-const Button = ({title, activeClass, _callBack}) => {
-  return (
-    <button style={{position:"relative", zIndex:"2"}}  onClick={_callBack}>{title}</button>
-  );
-}
-export default Button;
