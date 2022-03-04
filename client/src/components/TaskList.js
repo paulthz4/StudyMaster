d0838ebaf9c936652b1b3a77aed2f6de672bf66c
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-03-04 12:09:12 -0700

    onChange handler completed

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -3,44 +3,57 @@ import Axios from 'axios';
 import Button from './Button';
 
 export function TaskList() {
-  //const [tasks, setTasks] = useState([]);
-  
-  let i = <div className="task-box">
-      <p ><input type="checkbox" /><input className="task-input" type="text" placeholder="Task name" /></p>
-    </div>;
-    const [task, setTask] = useState("");
+    const [task1, setTask1] = useState("");
+    const [task2,setTask2] = useState("");
+    const [task3, setTask3] =  useState("");
+    
     const submitTask = () =>{
-      Axios.post("http://localhost:3001/api/insert", {taskName: task}).then(()=>{
+      console.log(task1+", "+task2+", "+task3)
+      Axios.post("http://localhost:3001/api/insert", {
+        taskName1: task1, 
+        taskName2: task2, 
+        taskName3: task3
+        })
+      .then(()=>{
         alert("sucessful insert");
+        
       });
+    }
+    
+    const handleChange = (e) =>{
+      console.log(task1);
+      switch(e.target.name){
+        case 'task1':
+          setTask1(e.target.value);
+          break;
+        case 'task2':
+          setTask2(e.target.value);
+          break;
+        case 'task3':
+          setTask3(e.target.value);
+          break;
+        default: break;
+      }  
     }
     return (
     <div className="task-container">
             <ul>
                 {/* {task.map((item, index) => <li id='i' key={index}>{item}</li>)} */}
                 <li><div className="task-box">
-                    <p ><input type="checkbox" /><input className="task-input" type="text" placeholder="Task name" /></p>
+                    <p ><input type="checkbox" /><input className="task-input" name='task1' onChange={handleChange} type="text" placeholder="Task name" /></p>
+                  </div>
+                </li>
+                <li><div className="task-box">
+                    <p ><input type="checkbox" /><input className="task-input" name='task2' onChange={handleChange} type="text" placeholder="Task Name" /></p>
                   </div>
                 </li>
                 
                 <li><div className="task-box">
-                    <p ><input type="checkbox" /><input className="task-input" type="text" placeholder="Task name" /></p>
+                    <p ><input type="checkbox" /><input className="task-input" name='task3'onChange={handleChange} type="text" placeholder="Task name" /></p>
                   </div>
                 </li>
-                
-                <li><div className="task-box">
-                    <p ><input type="checkbox" /><input className="task-input" type="text" placeholder="Task name" /></p>
-                  </div>
-                </li>
-                <li><button style={{fontSize:"small", width:"auto", height:"auto"}} onClick={submitTask}>Submit to database</button></li>
+                <li><button style={{fontSize:"small", width:"auto", height:"auto"}} onClick={submitTask}>Submit</button></li>
             </ul>
-            
-      {/*<div>
-                <button onClick={addTask} className="small-btns" >Add Task</button>
-      </div>
-            <div class="">
-                <button style={{ width: "fit-content" }} className="small-btns" onCLick={removeTasks} > Remove Finished Tasks</button>
-      </div>*/}
     </div>
   );
 }
