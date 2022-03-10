2f2989bf92163a83fc601e2a571b2f8473ced3fe
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-03-10 14:25:39 -0700

    only uses one state variable

--- client/src/components/CurrentTasks.js
+++ client/src/components/CurrentTasks.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Axios from 'axios';
-import Button from './Button';
+// import Button from './Button';
 
 export function CurrentTasks() {
-    const [task1, setTask1] = useState("");
-    const [task2,setTask2] = useState("");
-    const [task3, setTask3] =  useState("");
+    const [tasks, setTasks] = useState({task1:"", task2:"", task3:""});
     
     const submitTask = () =>{
-      console.log(task1+", "+task2+", "+task3)
       Axios.post("http://localhost:3001/api/insert", {
-        taskName1: task1, 
-        taskName2: task2, 
-        taskName3: task3
+        taskName1: tasks.task1, 
+        taskName2: tasks.task2, 
+        taskName3: tasks.task3
         })
       .then(()=>{
         alert("sucessful insert");
@@ -21,32 +18,21 @@ export function CurrentTasks() {
     }
     
     const handleChange = (e) =>{
-      switch(e.target.name){
-        case 'task1':
-          setTask1(e.target.value);
-          break;
-        case 'task2':
-          setTask2(e.target.value);
-          break;
-        case 'task3':
-          setTask3(e.target.value);
-          break;
-        default: break;
-      }  
+      setTasks(prevTasks =>({...prevTasks, [e.target.name]:e.target.value}));
+      console.log(tasks.task1,tasks.task2,tasks.task3); 
     }
     return (
     <div className="task-container">
             <ul>
-                <li><div className="task-box">
+                <li key="task1"><div className="task-box">
                     <p ><input type="checkbox" /><input className="task-input" name='task1' onChange={(e)=>handleChange(e)} type="text" placeholder="Task name" /></p>
                   </div>
                 </li>
-                <li><div className="task-box">
+                <li key="task2"><div className="task-box">
                     <p ><input type="checkbox" /><input className="task-input" name='task2' onChange={(e)=>handleChange(e)} type="text" placeholder="Task Name" /></p>
                   </div>
                 </li>
-                
-                <li><div className="task-box">
+                <li key="task3"><div className="task-box">
                     <p ><input type="checkbox" /><input className="task-input" name='task3'onChange={(e)=>handleChange(e)} type="text" placeholder="Task name" /></p>
                   </div>
                 </li>
