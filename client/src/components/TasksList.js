885485c856228cbe584cf785c0601c6b281de7e1
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-04-06 15:58:00 -0600

    TaskList renders only when > 0

--- client/src/components/TasksList.js
+++ client/src/components/TasksList.js
@@ -8,13 +8,16 @@ function CurrentTasks(props){
     
   }
   return(
-    <ul id="task-list">
-              {props.tasksList.map((val, index) =>{
-                  return <li key={val.idtasks} ><span>Task Name:</span> {val.taskName} <button className="task-list-button" id={val.idtasks} onClick={() => deleteTask(val.idtasks)}>Delete</button></li>
-                })
-              }
-              <li><button style={{fontSize:"small", width:"auto", height:"auto"}} onClick={props.onDelete}>Delete All</button></li>
-            </ul>
+  <> {props.tasksList.length > 0 &&
+      <ul id="task-list">
+                {props.tasksList.map((val, index) =>{
+                    return <li key={val.idtasks} ><span>Task Name:</span> {val.taskName} <button className="task-list-button" id={val.idtasks} onClick={() => deleteTask(val.idtasks)}>Delete</button></li>
+                  })
+                }
+                <li><button style={{fontSize:"small", width:"auto", height:"auto"}} onClick={props.onDelete}>Delete All</button></li>
+              </ul>
+      }
+  </>            
   );
 }
 export default CurrentTasks;
